f32c0924280435b9a3b81a3635aa226ecb9da763
Alex Krowitz <akrowitz@kronos.com>
2014-06-22 13:28:46 -0400

    Added CodeBook.md to check formatting.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ tidy-dataset
 ============
 
 The course project for Coursera's course on Getting and Cleaning Data. Everything needed to create a tidy dataset from wearable data.
+
+
+
+Decisions
+---------
+The tidy dataset reflects decisions that were made to resolve ambiguity or to
+provide clarity. The following list explains these decisions.
+
+1. Creation of the CodeBook from scratch rather than starting from the original data set's code book.
+  * The [Evaluation/Feedback section](https://class.coursera.org/getdata-004/human_grading/view/courses/972137/assessments/3/submissions) asks for a code book that *modifies and updates the codebooks available to you*.
+  * A dissenting opinion appears in a [TA response](https://class.coursera.org/getdata-004/forum/thread?thread_id=271#post-1639): *You are creating a codebook from scratch and describe what transformations or renaming you did as part of text*
+  * I chose to follow the TA post. My reasoning is that the original codebook is available for reference, and including information from it would complicate explanation of the tidy dataset. The original information is not necessarily pertinent, but is available onm the web if needed.
+  
--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,7 @@
 ## FAQ: https://class.coursera.org/getdata-004/forum/thread?thread_id=106
 ## June 2014
 
-date() # "Mon Jun 09 11:52:10 2014"
+date() # "Sun Jun 22 10:20:32 2014"
 
 ##
 ## 1. Merge the training and the test sets to create one data set
@@ -14,13 +14,16 @@ features <- # 561 observations of 2 variables
   fread("UCI HAR Dataset\\features.txt", header=FALSE)
 setnames(features, 1:2, c("feature_column_index", "feature_name"))
 
+# Feature names will be used as column names, so we make them valid
+features$feature_name <- make.names(features$feature_name, unique=TRUE)
+
 if (! identical(features$feature_column_index, 
                 seq(1,nrow(features))))
   stop("ERROR, features.txt: This script assumes the feature names 
         are completely listed in order of the corresponding data columns.")
 
 ##
-## 1.a  Training Data and Activities
+## 1.a  Training Data, Activities, and Subjects
 ##
 
 # read.table() is used because fread doesn't yet handle leading spaces
@@ -37,12 +40,18 @@ if (nrow(features) != ncol(trainMeasurements))
 
 trainActivities <-  # 7352 observations of 1 variable
     fread("UCI HAR Dataset\\train\\y_train.txt", header=FALSE)
-setNames(trainActivities, "labelID")
+setNames(trainActivities, "activityID")
 
 train <- cbind(trainMeasurements, trainActivities)
 
+trainSubjects <-  # 7352 observations of 1 variable
+  fread("UCI HAR Dataset\\train\\subject_train.txt", header=FALSE)
+setNames(trainSubjects, "subject")
+
+train <- cbind(train, trainSubjects)
+
 ##
-## 1.b Testing Data and Activities
+## 1.b Testing Data, Activities, and Subjects
 ##
 
 testMeasurements <- as.data.table( # 7352 observations of 561 variables
@@ -55,9 +64,15 @@ if (nrow(features) != ncol(testMeasurements))
 
 testActivities <-  # 7352 observations of 1 variable
   fread("UCI HAR Dataset\\test\\y_test.txt", header=FALSE)
-setNames(testActivities, "labelID")
+setNames(testActivities, "activityID")
 
 test <- cbind(testMeasurements, testActivities)
+
+testSubjects <-  # 7352 observations of 1 variable
+  fread("UCI HAR Dataset\\test\\subject_test.txt", header=FALSE)
+setNames(testSubjects, "subject")
+
+test <- cbind(test, testSubjects)
 
 ##
 ## 1.c Combine Train and Test into one table, identifying which is which
@@ -83,9 +98,11 @@ meanAndStdColNums <- grep("mean|std", features$feature_name, ignore.case = TRUE)
 keptFeatureNames <- features$feature_name[meanAndStdColNums]
 keptFeatureColNames <- names(trainAndTest)[meanAndStdColNums]  # V1, V2,V46, ...
 
-# We want to keep the activity label column and the train/test identification
+# We want to keep the activity label column, the subject,
+# and the train/test identification
 keepCols <- c(meanAndStdColNums, 
-              which(names(trainAndTest)=="labelID"),
+              which(names(trainAndTest)=="activityID"),
+              which(names(trainAndTest)=="subject"),
               which(names(trainAndTest)=="isTestData"))
 trainAndTest <- trainAndTest[,keepCols,with=FALSE]
 
@@ -95,26 +112,45 @@ trainAndTest <- trainAndTest[,keepCols,with=FALSE]
 
 activityNames <- 
   fread("UCI HAR Dataset\\activity_labels.txt", header=FALSE)
-setnames(activityNames, 1:2, c("labelID", "labelName"))
-setkey(activityNames, labelID)
+setnames(activityNames, 1:2, c("activityID", "activity"))
+setkey(activityNames, activityID)
 
 # Replace activity IDs with activity names
-setkey(trainAndTest,labelID)
+setkey(trainAndTest,activityID)
 trainAndTest <- activityNames[trainAndTest]
 
-# We don't need the labelID anymore
-trainAndTest[,labelID:=NULL]
+# We don't need the activityID anymore
+trainAndTest[,activityID:=NULL]
 
 ##
 ## 4. Appropriately label the data set with descriptive variable names
 ##
 
-setnames(train, old=keptFeatureColNames, new=keptFeatureNames)
+setnames(trainAndTest, old=keptFeatureColNames, new=keptFeatureNames)
 
 ##
-## 5. Creates a second, independent tidy data set with the average of each
-## variable for each activity and each subject.
+## 5. Create a second, independent tidy data set with the average of each
+## variable for each activity and each subject
 ##
 
+## 
+## 5.a Create the averages. 
+## Note that each subject is either a training subject 
+## or a testing subject, but never both. Grouping by activity, subject, and 
+## isTestData therefore creates the same averages as grouping by just activity 
+## and subject.
+## 
+tidydata <- 
+  trainAndTest[,lapply(.SD,mean), by=list(activity, subject, isTestData)]
 
+## 
+## 5.b Append ".Avg" to measurement names to indicate by-activity/subject
+## averages
+##
+avgNames <- sapply(keptFeatureNames, function(name) { paste0(name, ".Avg") })
+setnames(tidydata, old=keptFeatureNames, new=avgNames)
 
+##
+## 5.c Write the tidy dataset
+##
+write.csv(tidydata, file="tidydata.csv")
